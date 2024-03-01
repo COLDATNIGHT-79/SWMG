9fa9e1f05289c5367b90dd54fd086c4756e6c8a8
AJ is_cool <ajnimator@gmail.com>
2024-03-01 08:43:54 +0530

    new

--- client/src/utils/client.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const baseURL =
-    process.env.NODE_ENV === "production"
-        ? "https://backend-socialmediaapp.herokuapp.com/api"
-        : "http://localhost:3000";
-
-const client = axios.create({ baseURL });
-export default client;
